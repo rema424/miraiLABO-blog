34ce438db221b09d462f733d19792f60229e7072
null-miraiLABO <mizuki2002b@gmail.com>
2021-07-18 22:47:31 +0900

    2th html response

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module miraiLABO-blog
 
 go 1.16
 
-require github.com/labstack/echo/v4 v4.4.0
+require (
+	github.com/flosch/pongo2 v0.0.0-20200913210552-0d938eb266f3
+	github.com/labstack/echo/v4 v4.4.0
+	golang.org/x/text v0.3.6
+)
--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 //利用するパッケージの宣言
 import (
 	"net/http" //標準パッケージ
+	"time"
 
 	//外部パッケージ
+	"github.com/flosch/pongo2"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	//
 )
+
+const tmplPath = "src/template/"
 
 //グローバル変数eにcreateMux()の関数の戻り値を格納
 var e = createMux()
@@ -32,6 +37,21 @@ func createMux() *echo.Echo {
 }
 
 func articleindex(c echo.Context) error {
-	//ステータスコード200で、"Hello, World!"と言う文字列をレスポンス
-	return c.String(http.StatusOK, "Hello, World!")
+	data := map[string]interface{}{
+		"Message": "Hello, World!",
+		"Now":     time.Now(),
+	}
+	return render(c, "article/index.html", data)
+}
+
+func htmlBlob(file string, data map[string]interface{}) ([]byte, error) {
+	return pongo2.Must(pongo2.FromCache(tmplPath + file)).ExecuteBytes(data)
+}
+
+func render(c echo.Context, file string, data map[string]interface{}) error {
+	b, err := htmlBlob(file, data)
+	if err != nil {
+		return c.NoContent(http.StatusInternalServerError)
+	}
+	return c.HTMLBlob(http.StatusOK, b)
 }
