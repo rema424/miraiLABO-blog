2081d220967ebe24329461734686b5dcb22fc3ba
null-miraiLABO <mizuki2002b@gmail.com>
2021-07-21 16:59:41 +0900

    5th commit

--- main.go
+++ main.go
@@ -2,14 +2,14 @@ package main
 
 //利用するパッケージの宣言
 import (
-	"net/http" //標準パッケージ
+	"net/http"
+	"strconv"
 	"time"
 
 	//外部パッケージ
 	"github.com/flosch/pongo2"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	//
 )
 
 const tmplPath = "src/template/"
@@ -20,6 +20,10 @@ var e = createMux()
 func main() {
 	//`/`と言うパス(URL)と``articleindex`を結びつける
 	e.GET("/", articleindex)
+	//ルーディング追加
+	e.GET("/new", articleNew)
+	e.GET("/:id", articleShow) //idはパスパラメーター
+	e.GET("/:id/edit", articleEdit)
 
 	//wevサーバーをローカルホストで起動する
 	e.Logger.Fatal(e.Start(":8080"))
@@ -42,10 +46,43 @@ func createMux() *echo.Echo {
 //データ渡し
 func articleindex(c echo.Context) error {
 	data := map[string]interface{}{
-		"Message": "Hello, World!",
+		"Message": "Article index",
 		"Now":     time.Now(),
 	}
 	return render(c, "article/index.html", data)
+}
+
+func articleNew(c echo.Context) error {
+	data := map[string]interface{}{
+		"Message": "Article New",
+		"Now":     time.Now(),
+	}
+	return render(c, "article/new.html", data)
+}
+
+func articleShow(c echo.Context) error {
+	//パスパラメーターを抽出
+	id, _ := strconv.Atoi(c.Param("id")) //c.Param()文字列型からAtoi()でキャスト変換
+
+	data := map[string]interface{}{
+		"Message": "Article Show",
+		"Now":     time.Now(),
+		"ID":      id,
+	}
+
+	return render(c, "article/show.html", data)
+}
+
+func articleEdit(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	data := map[string]interface{}{
+		"Message": "Article Edit",
+		"Now":     time.Now(),
+		"ID":      id,
+	}
+
+	return render(c, "article/edit.html", data)
 }
 
 //生成したhtmlデータをバイトデータとして返す
